72a0dc6ba14c269b9d6d0ed881a0739db68a4563
sakkarakaya <mehmet-sakib.karakaya@stud.hs-hannover.de>
2020-11-17 16:26:01 +0100

    menu+search

--- Gruppenarbeit/js/menu.js
+++ Gruppenarbeit/js/menu.js
@@ -4,3 +4,8 @@
  * when the user clicks on the element with the id "menu-btn"
  * 
  */
+
+document.getElementById("menu-btn").addEventListener("click", f3);
+function f3(){
+    document.getElementById("menu").classList.toggle("hidden");
+ }
--- Gruppenarbeit/js/search.js
+++ Gruppenarbeit/js/search.js
@@ -1,6 +1,19 @@
 /**
+ * 
  * CSS-ANIMATIONS
  * 1. Toggle the class "hidden" from the searchbar element, when the user clicks search button
  * 2. Clear the input by clicking the "x" button
  * 
  */
+
+
+ //document.getElementsByClassName("search-btn").addEventListener("click", f);
+ function f(){
+    document.getElementById("searchbar").classList.toggle("hidden");
+ }
+
+
+ //document.getElementById("x").addEventListener("click", f2);
+ function f2(){
+    document.getElementById("searcharea").value = "";
+ }
